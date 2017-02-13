d519e058fde80dfa6bc327c39ec2033ecdf508bf
Gergely Daroczi <gergely.daroczi@card.com>
2017-02-13 12:57:21 +0000

    version 1.11.89

--- R/zzz.R
+++ R/zzz.R
@@ -1,38 +1,75 @@
 #' Installing AWS SDK jars for R
 #'
-#' Downloads and installs the AWS SDK for Java jars from the \code{ARW.jars} packages, which also adds to the Java classpath to be easily used in other R packages.
+#' Downloads and installs the AWS SDK for Java jars and adds those to the Java classpath to be easily used in other R packages.
+#'
+#' The first time the package is loaded (which should happen in most cases automatically right after installation), this will check if the jar files are available and if not, then the package will try to automatically download  the specific version of jar files from the \url{https://gitlab.com/cardcorp/AWR} private repository and push the files to the local \pkg{AWR} package's installation folder. If it does not succeed, please install the package directly from the private repository via \code{install.packages("AWR", repos = "https://cardcorp.gitlab.io/AWR")}, which will download the jars bundled with the source package, so no separate downloads will be required. This is also suggested if you want to run the most recent version of the AWS SDK, as there is a new AWS SDK patch version rolled out almost every other day, while the CRAN updates happen less frequently.
 #' @references \url{https://aws.amazon.com/sdk-for-java}
 #' @docType package
 #' @importFrom rJava .jpackage
-#' @importFrom utils install.packages packageVersion
+#' @importFrom utils packageVersion unzip download.file
 #' @name AWR-package
 #' @examples \dontrun{
-#' ## adding the jars to the Java classpath
 #' library(rJava)
-#'
-#' ## creating a client in Java
 #' kc <- .jnew("com.amazonaws.services.s3.AmazonS3Client")
-#' ## listing the account name
 #' kc$getS3AccountOwner()$getDisplayName()
 #' }
 NULL
 
 .onLoad <- function(libname, pkgname) {
 
-    ## check if the AWS Java SDK jars are available and install if needed
-    if (requireNamespace('AWR.jars', quietly = TRUE) == FALSE) {
-        warning('AWR.jars package not found, installing now...')
-        install.packages('AWR.jars', repos = 'https://cardcorp.gitlab.io/AWR.jars')
+    ## path to the jars
+    path <- file.path(system.file('', package = pkgname), 'java')
+
+    ## check if the AWS Java SDK jars are available and install if needed (on first load)
+    if (length(list.files(path)) == 0) {
+
+        ## download the package from the private repo (storing the jars as well) to a temp location
+        tzip <- tempfile()
+        dres <- tryCatch(download.file(
+            url = sprintf(
+                'https://gitlab.com/cardcorp/AWR/repository/archive.zip?ref=%s',
+                packageVersion('AWR')),
+            destfile = tzip, mode = 'wb'), error = function(e) e)
+
+        ## create the java folder in the package install folder if not yet available
+        if (!dir.exists(path)) {
+            dir.create(path, recursive = TRUE)
+        }
+
+        ## compile a full list of files in the zip archive (also good for checkin)
+        files <- tryCatch(unzip(tzip, list = TRUE), error = function(e) e)
+
+        ## skip unzipping when there's a problem with the download
+        if (!inherits(dres, 'error') && dres == 0 && !inherits(files, 'error')) {
+
+            ## unzip the list of jars to the java folder
+            files <- grep('.jar$', files$Name, value = TRUE)
+            unzip(tzip, files = files, exdir = path, junkpaths = TRUE)
+
+        }
+
+        ## clean up temp files
+        unlink(tzip)
+
     }
 
-    ## make sure we have a recent version of the Java SDK
-    if (packageVersion('AWR') > packageVersion('AWR.jars')) {
-        warning(paste('AWR.jars package is older than current AWR package,',
-                      'installing most recent version of the jars now...'))
-        install.packages('AWR.jars', repos = 'https://cardcorp.gitlab.io/AWR.jars')
-    }
+    ## add the jars to the Java classpath
+    .jpackage(pkgname, lib.loc = libname,
+              ## for devtools::load_all in the development environment
+              morePaths = list.files(system.file('java', package = pkgname), full.names = TRUE))
 
-    ## add jars to the Java classpath
-    .jpackage('AWR.jars')
+}
+
+.onAttach <- function(libname, pkgname) {
+
+    ## let the user know if the AWR jar installation failed and provide hints on how to fix it
+    path <- file.path(system.file('', package = pkgname), 'java')
+    if (length(list.files(path)) == 0) {
+        packageStartupMessage(
+            'The automatic installation of the AWR jars seems to have failed.\n',
+            'Please check your Internet connection and if the current user can write to ', path, '\n',
+            'If still having issues, install the package from the private repo also including the jars:\n',
+            '\ninstall.packages("AWR", repos = "https://cardcorp.gitlab.io/AWR")\n')
+    }
 
 }
--- README.md
+++ README.md
@@ -1,22 +1,40 @@
 # AWR
 
-This R package installs the [AWS SDK for Java](https://aws.amazon.com/sdk-for-java) `jar` files from [AWR.jars](https://gitlab.com/cardcorp/AWR.jars) to be used in downstream R packages.
+This R package bundles the [AWS SDK for Java](https://aws.amazon.com/sdk-for-java) `jar` files to be used in downstream R packages.
 
 ## Why the name?
 
-This is an R package bundling AWS jars, but S is so 1992.
+This is an R package to interact with AWS, but S is so 1992.
 
 ## What is it good for?
 
 The bundled Java SDK is useful for R package developers working with AWS so that they can easily import this package to get access to the Java `jar` files. Quick example on using the Amazon S3 Java client:
 
 ```r
-> ## adding the jars to the Java classpath
 > library(rJava)
-
-> ## creating a client in Java
-> kc <- .jnew("com.amazonaws.services.s3.AmazonS3Client")
-> ## listing the account name
+> kc <- .jnew('com.amazonaws.services.s3.AmazonS3Client')
 > kc$getS3AccountOwner()$getDisplayName()
 [1] "foobar"
+```
+
+## Installation
+
+![CRAN version](http://www.r-pkg.org/badges/version-ago/AWR)
+
+The package is hosted on [CRAN](https://cran.r-project.org/package=AWR), so installation is as easy as:
+
+```r
+install.packages('AWR')
+```
+
+But you can similarly easily install the most recent version of the SDK from the development git repository as well thanks to `drat`:
+
+```r
+install.packages('AWR', repos = 'https://cardcorp.gitlab.io/AWR')
+```
+
+If you want to install a specific version of the AWS SDK, then refer to the version tag, eg installing `1.11.76`:
+
+```r
+install.packages('https://gitlab.com/cardcorp/AWR/repository/archive.tar.gz?ref=1.11.76', repos = NULL)
 ```
